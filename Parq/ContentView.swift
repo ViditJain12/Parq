3d10d0e6df7dfec5da585b2ceda3088b8f5f9ec8
Vidit Jain <viditjain2005@gmail.com>
2026-04-29 17:10:59 -0700

    Initial Commit

---
+++ Parq/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parq
+//
+//  Created by Vidit Jain on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parq/ParqApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParqApp.swift
+//  Parq
+//
+//  Created by Vidit Jain on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ParqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
